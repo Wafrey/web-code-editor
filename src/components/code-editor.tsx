78258965ad7de56c0f2a849c922f0599131cfb1a
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2021-12-08 22:28:52 +0200

    implement browser bundilng. fetching and loading modules. displaying code editor in the app.

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,38 @@
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+  };
+
+  return (
+    <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      value={initialValue}
+      theme='dark'
+      language='javascript'
+      height='500px'
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
+};
+
+export default CodeEditor;
