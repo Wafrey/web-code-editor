629a776ef75e64f02e12dd3d667613c3bb8e99bc
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2022-01-09 16:45:38 +0200

    Add redux and create the store

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, useRef } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+import './text-editor.css';
+
+const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [value, setValue] = useState('# Header');
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div className='text-editor' ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
+      </div>
+    );
+  }
+
+  return (
+    <div className='text-editor card' onClick={() => setEditing(true)}>
+      <div className='card-content'>
+        <MDEditor.Markdown source={value} />
+      </div>
+    </div>
+  );
+};
+
+export default TextEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import ReactDOM from 'react-dom';
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
+import { store } from './state';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
   return (
-    <div>
-      <CodeCell />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
