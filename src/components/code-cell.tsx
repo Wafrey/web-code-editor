2e4405e0fb7820a05c020d03b4d18d5c0d27cb66
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2022-03-08 16:15:36 +0200

    update code-cell loader

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -45,15 +45,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {!bundle || bundle.loading ? (
-          <div className='progress-cover'>
-            <progress className='progress is-small is-primary' max='100'>
-              Loading
-            </progress>
-          </div>
-        ) : (
-          <Preview code={bundle.code} err={bundle.err} />
-        )}
+        <div className='progress-wrapper'>
+          {!bundle || bundle.loading ? (
+            <div className='progress-cover'>
+              <progress className='progress is-small is-primary' max='100'>
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} err={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
