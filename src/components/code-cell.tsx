e4f26b1d31cc1f8ff0fdf81d5085e9d27521738b
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2022-03-10 17:41:15 +0200

    Add showing react components to code-cell

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -18,7 +18,25 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCode = [];
+    const cumulativeCode = [
+      `
+        import React from 'react';
+        import ReactDOM from 'react-dom';
+        const show = (value) => {
+          const root = document.querySelector('#root');
+
+          if (typeof value === 'object') {
+            if (value.$$typeof && value.props) {
+              ReactDOM.render(value, root);
+            } else {
+              root.innerHTML = JSON.stringify(value);
+            }
+          } else {
+            root.innerHTML = value;
+          }
+        };
+      `,
+    ];
     for (let c of orderedCells) {
       if (c.type === 'code') {
         cumulativeCode.push(c.content);
